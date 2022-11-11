10017658246cd3b539e6576c0680305aa63d6f01
Art Code <90895680+yosytone@users.noreply.github.com>
2022-11-11 23:37:31 +0300

    Create lab2.pl

---
+++ lab2.pl
@@ -0,0 +1,338 @@
+born_date(omar_sy, 0).
+born_date(keanu_reeves, 0).
+born_date(will_smith, 0).
+born_date(matthew_mc_conaughey, 0).
+born_date(edward_norton, 0).
+born_date(jared_leto, 0).
+born_date(bradley_cooper, 0).
+born_date(leonardo_di_caprio, 0).
+born_date(brad_pitt, 0).
+born_date(angelina_jolie, 0).
+born_date(johnny_depp, 0).
+born_date(robert_downey_jr, 0).
+born_date(ursula_andress, 1).
+born_date(steven_spielberg, 1).
+born_date(harrison_ford, 1).
+born_date(jim_carrey, 1).
+born_date(clint_eastwood, 1).
+born_date(jackie_chan, 1).
+born_date(morgan_freeman, 1).
+born_date(willem_defoe, 1).
+born_date(robert_de_niro, 1).
+born_date(bruce_whillis, 1).
+born_date(tom_cruise, 1).
+
+number_of_films(omar_sy, 0).
+number_of_films(keanu_reeves, 0).
+number_of_films(will_smith, 0).
+number_of_films(matthew_mc_conaughey, 0).
+number_of_films(edward_norton, 0).
+number_of_films(jared_leto, 0).
+number_of_films(bradley_cooper, 0).
+number_of_films(leonardo_di_caprio, 0).
+number_of_films(brad_pitt, 0).
+number_of_films(angelina_jolie, 0).
+number_of_films(johnny_depp, 1).
+number_of_films(robert_downey_jr, 1).
+number_of_films(ursula_andress, 0).
+number_of_films(steven_spielberg, 0).
+number_of_films(harrison_ford, 0).
+number_of_films(jim_carrey, 0).
+number_of_films(clint_eastwood, 0).
+number_of_films(jackie_chan, 1).
+number_of_films(morgan_freeman, 1).
+number_of_films(willem_defoe, 1).
+number_of_films(robert_de_niro, 1).
+number_of_films(bruce_whillis, 1).
+number_of_films(tom_cruise, 1).
+
+golden_raspberry(omar_sy, 0).
+golden_raspberry(keanu_reeves, 0).
+golden_raspberry(will_smith, 0).
+golden_raspberry(matthew_mc_conaughey, 0).
+golden_raspberry(edward_norton, 0).
+golden_raspberry(jared_leto, 0).
+golden_raspberry(bradley_cooper, 0).
+golden_raspberry(leonardo_di_caprio, 0).
+golden_raspberry(brad_pitt, 0).
+golden_raspberry(angelina_jolie, 0).
+golden_raspberry(johnny_depp, 0).
+golden_raspberry(robert_downey_jr, 0).
+golden_raspberry(ursula_andress, 0).
+golden_raspberry(steven_spielberg, 0).
+golden_raspberry(harrison_ford, 0).
+golden_raspberry(jim_carrey, 0).
+golden_raspberry(clint_eastwood, 0).
+golden_raspberry(jackie_chan, 0).
+golden_raspberry(morgan_freeman, 0).
+golden_raspberry(willem_defoe, 0).
+golden_raspberry(robert_de_niro, 0).
+golden_raspberry(bruce_whillis, 1).
+golden_raspberry(tom_cruise, 1).
+
+nominations(omar_sy, 0).
+nominations(keanu_reeves, 0).
+nominations(will_smith, 0).
+nominations(matthew_mc_conaughey, 0).
+nominations(edward_norton, 0).
+nominations(jared_leto, 0).
+nominations(bradley_cooper, 1).
+nominations(leonardo_di_caprio, 1).
+nominations(brad_pitt, 1).
+nominations(angelina_jolie, 1).
+nominations(johnny_depp, 0).
+nominations(robert_downey_jr, 1).
+nominations(ursula_andress, 0).
+nominations(steven_spielberg, 0).
+nominations(harrison_ford, 0).
+nominations(jim_carrey, 0).
+nominations(clint_eastwood, 1).
+nominations(jackie_chan, 0).
+nominations(morgan_freeman, 0).
+nominations(willem_defoe, 0).
+nominations(robert_de_niro, 1).
+nominations(bruce_whillis, 0).
+nominations(tom_cruise, 1).
+
+citizenship(omar_sy, 0).
+citizenship(keanu_reeves, 0).
+citizenship(will_smith, 0).
+citizenship(matthew_mc_conaughey, 0).
+citizenship(edward_norton, 0).
+citizenship(jared_leto, 0).
+citizenship(bradley_cooper, 0).
+citizenship(leonardo_di_caprio, 0).
+citizenship(brad_pitt, 0).
+citizenship(angelina_jolie, 1).
+citizenship(johnny_depp, 0).
+citizenship(robert_downey_jr, 0).
+citizenship(ursula_andress, 0).
+citizenship(steven_spielberg, 0).
+citizenship(harrison_ford, 0).
+citizenship(jim_carrey, 1).
+citizenship(clint_eastwood, 0).
+citizenship(jackie_chan, 0).
+citizenship(morgan_freeman, 0).
+citizenship(willem_defoe, 1).
+citizenship(robert_de_niro, 1).
+citizenship(bruce_whillis, 1).
+citizenship(tom_cruise, 0).
+
+born_location(omar_sy, 0).
+born_location(keanu_reeves, 0).
+born_location(will_smith, 0).
+born_location(matthew_mc_conaughey, 1).
+born_location(edward_norton, 1).
+born_location(jared_leto, 1).
+born_location(bradley_cooper, 1).
+born_location(leonardo_di_caprio, 1).
+born_location(brad_pitt, 1).
+born_location(angelina_jolie, 1).
+born_location(johnny_depp, 1).
+born_location(robert_downey_jr, 1).
+born_location(ursula_andress, 0).
+born_location(steven_spielberg, 0).
+born_location(harrison_ford, 1).
+born_location(jim_carrey, 0).
+born_location(clint_eastwood, 1).
+born_location(jackie_chan, 0).
+born_location(morgan_freeman, 1).
+born_location(willem_defoe, 1).
+born_location(robert_de_niro, 1).
+born_location(bruce_whillis, 0).
+born_location(tom_cruise, 1).
+
+career_start(omar_sy, 0).
+career_start(keanu_reeves, 0).
+career_start(will_smith, 0).
+career_start(matthew_mc_conaughey, 0).
+career_start(edward_norton, 0).
+career_start(jared_leto, 0).
+career_start(bradley_cooper, 0).
+career_start(leonardo_di_caprio, 0).
+career_start(brad_pitt, 0).
+career_start(angelina_jolie, 1).
+career_start(johnny_depp, 0).
+career_start(robert_downey_jr, 1).
+career_start(ursula_andress, 1).
+career_start(steven_spielberg, 1).
+career_start(harrison_ford, 1).
+career_start(jim_carrey, 1).
+career_start(clint_eastwood, 1).
+career_start(jackie_chan, 1).
+career_start(morgan_freeman, 1).
+career_start(willem_defoe, 1).
+career_start(robert_de_niro, 1).
+career_start(bruce_whillis, 1).
+career_start(tom_cruise, 1).
+
+producer(omar_sy, 0).
+producer(keanu_reeves, 1).
+producer(will_smith, 1).
+producer(matthew_mc_conaughey, 0).
+producer(edward_norton, 1).
+producer(jared_leto, 1).
+producer(bradley_cooper, 0).
+producer(leonardo_di_caprio, 1).
+producer(brad_pitt, 1).
+producer(angelina_jolie, 1).
+producer(johnny_depp, 1).
+producer(robert_downey_jr, 1).
+producer(ursula_andress, 0).
+producer(steven_spielberg, 1).
+producer(harrison_ford, 1).
+producer(jim_carrey, 1).
+producer(clint_eastwood, 1).
+producer(jackie_chan, 1).
+producer(morgan_freeman, 1).
+producer(willem_defoe, 1).
+producer(robert_de_niro, 1).
+producer(bruce_whillis, 1).
+producer(tom_cruise, 1).
+
+awards(omar_sy, 0).
+awards(keanu_reeves, 0).
+awards(will_smith, 1).
+awards(matthew_mc_conaughey, 1).
+awards(edward_norton, 0).
+awards(jared_leto, 0).
+awards(bradley_cooper, 1).
+awards(leonardo_di_caprio, 1).
+awards(brad_pitt, 1).
+awards(angelina_jolie, 1).
+awards(johnny_depp, 1).
+awards(robert_downey_jr, 1).
+awards(ursula_andress, 0).
+awards(steven_spielberg, 1).
+awards(harrison_ford, 1).
+awards(jim_carrey, 1).
+awards(clint_eastwood, 1).
+awards(jackie_chan, 1).
+awards(morgan_freeman, 1).
+awards(willem_defoe, 1).
+awards(robert_de_niro, 1).
+awards(bruce_whillis, 1).
+awards(tom_cruise, 1).
+
+musician(omar_sy, 0).
+musician(keanu_reeves, 1).
+musician(will_smith, 1).
+musician(matthew_mc_conaughey, 0).
+musician(edward_norton, 0).
+musician(jared_leto, 1).
+musician(bradley_cooper, 0).
+musician(leonardo_di_caprio, 0).
+musician(brad_pitt, 0).
+musician(angelina_jolie, 0).
+musician(johnny_depp, 1).
+musician(robert_downey_jr, 1).
+musician(ursula_andress, 0).
+musician(steven_spielberg, 0).
+musician(harrison_ford, 0).
+musician(jim_carrey, 0).
+musician(clint_eastwood, 0).
+musician(jackie_chan, 1).
+musician(morgan_freeman, 0).
+musician(willem_defoe, 0).
+musician(robert_de_niro, 0).
+musician(bruce_whillis, 1).
+musician(tom_cruise, 0).
+
+birthday_season(omar_sy, 1).
+birthday_season(keanu_reeves, 0).
+birthday_season(will_smith, 0).
+birthday_season(matthew_mc_conaughey, 0).
+birthday_season(edward_norton, 0).
+birthday_season(jared_leto, 1).
+birthday_season(bradley_cooper, 1).
+birthday_season(leonardo_di_caprio, 0).
+birthday_season(brad_pitt, 1).
+birthday_season(angelina_jolie, 0).
+birthday_season(johnny_depp, 1).
+birthday_season(robert_downey_jr, 0).
+birthday_season(ursula_andress, 0).
+birthday_season(steven_spielberg, 1).
+birthday_season(harrison_ford, 0).
+birthday_season(jim_carrey, 1).
+birthday_season(clint_eastwood, 1).
+birthday_season(jackie_chan, 0).
+birthday_season(morgan_freeman, 0).
+birthday_season(willem_defoe, 0).
+birthday_season(robert_de_niro, 0).
+birthday_season(bruce_whillis, 0).
+birthday_season(tom_cruise, 0).
+
+question1(X):-
+	write("The actor was born in 1962 or earlier?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question2(X):-
+	write("Has acted in over 90 films?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question3(X):-
+	write("Won the Golden Raspberry Award for Worst Actor?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question4(X):-
+	write("More than 25 nominations?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question5(X):-
+	write("More than 1 citizenship?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question6(X):-
+	write("Born in the USA?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question7(X):-
+	write("Career start in 1982 or earlier?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question8(X):-
+	write("Producer?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question9(X):-
+	write("4 or more awards?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question10(X):-
+	write("Musician?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+question11(X):-
+	write("Born in winter?"),nl,
+	write("1. Yes"),nl,
+	write("0. NO"),nl,
+	read(X).
+
+pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
+	question5(X5),question6(X6),question7(X7),
+	question8(X8),question9(X9),question10(X10),
+	question11(X11),
+	born_date(X,X1),number_of_films(X,X2),golden_raspberry(X,X3),nominations(X,X4),
+	citizenship(X,X5),born_location(X,X6),career_start(X,X7),producer(X,X8),
+	awards(X,X9),musician(X,X10),birthday_season(X,X11),
+	write(X).
