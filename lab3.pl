e5655fae3e43429af597f66f4834d30e2251e9ef
Art Code <90895680+yosytone@users.noreply.github.com>
2022-11-19 11:45:13 +0300

    Update lab3.pl

--- lab3.pl
+++ lab3.pl
@@ -26,7 +26,7 @@ factorial(N,X):-fact(N,1,X).
 fib(0,0):-!.
 fib(1,1):-!.
 fib(2,1):-!.
-fib(N,X):- N1 is N-1, N2 is N-2, fib(N1,X1), fib(N2,X2), X is X1+X2.
+%fib(N,X):- N1 is N-1, N2 is N-2, fib(N1,X1), fib(N2,X2), X is X1+X2.
 
 
 %Реализовать предикат fib(N,X), где X – число Фибоначчи с
